e09cb3424c50e8b9a8f9fc2c358401800d3b6387
Victor Piero Limache Victorio <71676154+VictorPiero01@users.noreply.github.com>
2021-12-02 18:55:09 -0500

    v1

---
+++ app.js
@@ -0,0 +1,42 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: false // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+      // SALUDOS
+    if ((message.body === 'Hola' || message.body === 'Buenos dias') && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Bienvenido a la Tienda Heppie')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+    if ((message.body === 'Boxer varon' || message.body === 'Calzoncillos varones disponibles') && message.isGroupMsg === false) {
+        client
+          //.sendText(message.from, 'Bienvenido a la Tienda Heppie')
+          .sendText(message.from, 'Los Productos disponibles de varones son:  ✅ Opción 1: Bóxer Calvin Klein Color solido ✅ Opción 2:Bóxer Calvin Klein Rayas anchas')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+
+
+  });
+}
